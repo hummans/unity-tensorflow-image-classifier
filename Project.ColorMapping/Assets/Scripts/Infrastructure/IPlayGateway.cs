03c519f4daff92c96ee819b1b3c66e2aa2a424a1
Matias A. Vallejos <matiasavallejos@outlook.com>
2021-11-25 09:31:54 -0300

    Update AR infrastructure to upload image to server

--- Project.ColorMapping/Assets/Scripts/Infrastructure/IPlayGateway.cs
+++ Project.ColorMapping/Assets/Scripts/Infrastructure/IPlayGateway.cs
@@ -7,7 +7,6 @@ namespace Infrastructure
 {
     public interface IPlayGateway 
     {
-        string URL_DATA {get; set;}
         public IObservable<Unit> PlayTurn(TrackManager trackImageManagerData, Texture2D textureScreenshoot);
     }
 }
