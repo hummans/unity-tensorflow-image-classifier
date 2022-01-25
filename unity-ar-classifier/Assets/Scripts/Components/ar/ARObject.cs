9ec8acf67cd9faf0f97df5352a82ab8417e77a41
Matias A. Vallejos <matiasavallejos@outlook.com>
2022-01-25 09:55:47 -0300

    Rename and correct screenshoot bug

--- unity-ar-classifier/Assets/Scripts/Components/ar/ARObject.cs
+++ unity-ar-classifier/Assets/Scripts/Components/ar/ARObject.cs
@@ -5,11 +5,15 @@ namespace Components
 {
     public class ARObject : MonoBehaviour
     {
+        public TrackManagerViewModel trackData;
         public ARObjectViewModel arObjectData;
         
         void Awake() 
         {
             arObjectData.objectScale = this.transform.localScale;
         }
+        
+        void OnEnable() => trackData.currentTrackInterfaceActive.Value = false;   
+        void OnDisable() => trackData.currentTrackInterfaceActive.Value = true;
     }
 }
